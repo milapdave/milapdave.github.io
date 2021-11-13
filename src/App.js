3533d10beef709ecb6313421bcefbb4b07781a18
Milap Dave <dave.milap3@gmail.com>
2021-11-13 11:31:19 +0530

    add content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Hello I’m Milap Dave, <br/>
+          Front end developer in Rajkot, Gujarat</h1>
+          
+          <div className="about_section">
+            <h3>ABOUT ME</h3>
+            <p class=""> I’m Milap Dave. <strong>Front end developer in Rajkot</strong>, Gujarat. I love to create beautiful, intuitive and functional design using the latest techniques with&nbsp;<a href="https://html.com/">HTML</a>&nbsp;/ CSS /<a href="http://sass-lang.com/">SASS</a>,&nbsp; jQuery. I love everything that is digital, in my spare time I like to experiments and tutorials or freebies for designers and developers.</p>
+            <p class="social-links link text-uppercase"> <a href="mailto:dave.milap3@gmail.com?Subject=Hello%20Milap">Email</a> <a rel="noreferrer noopener" href="https://github.com/milapdave" target="_blank">github</a> <a rel="noreferrer noopener" href="https://www.linkedin.com/in/milapdave/" target="_blank">linkedin</a> <a rel="noreferrer noopener" href="https://medium.com/@milapdave" target="_blank">medium </a> <a rel="noreferrer noopener" href="https://twitter.com/milap_dave" target="_blank">twitter</a></p>
+          </div>
       </header>
     </div>
   );
